b41b3c91683724c81ec9a9c506ea6477fdea932a
Hakim <hakimkavuma99@gmail.com>
2025-05-08 22:34:20 +0300

    feat: update LoginForm component with new company name and enhanced styling

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,13 @@
-
 import Logo from "../assets/react.svg"
 
-const companyName: string = "YYY STATISTICS";
+const companyName: string = "MoneyMind";
 interface Styles {
     container: React.CSSProperties;
     logo: React.CSSProperties;
     forms: React.CSSProperties;
-    
+    submit: React.CSSProperties;
+    input: React.CSSProperties;
+
 }
 
 const styles: Styles = {
@@ -14,8 +15,8 @@ const styles: Styles = {
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
-        gap: "20px",
-
+        alignItems: "center",
+        gap: "20px"
     },
     logo: {
         width: "100px",
@@ -25,30 +26,48 @@ const styles: Styles = {
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
+        alignItems: "center",
         gap: "10px",
         marginTop: "20px",
         width: "300px",
         padding: "20px"
+    },
+    submit: {
+        backgroundColor: "red",
+        color: "white",
+        padding: "10px",
+        border: "none",
+        cursor: "pointer",
+        fontSize: "16px",
+        fontWeight: "bold",
+        width: "100px",
+        alignSelf: "center",
+    },
+    input: {
+        padding: "10px",
+        borderRadius: "5px",
+        border: "1px solid #ccc",
+        width: "100%"
     }
-}
-const Headings = () =>{
-    return(
-        <div style={styles.container}>
-            <img src={Logo} style={styles.logo}></img>
-            <h1>{companyName}</h1>
-            
-        </div>
-        
-    )
 }
 
 const LoginForm = () => {
     return(
-        <div style={styles.container}>
-            <form style={styles.forms}>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px"}}>
+            <div style={styles.container}>
+                <img src={Logo} style={styles.logo}></img>
+                <h1>{companyName}</h1>
+            </div>
+            <form style={styles.forms} action="POST">
+                <input type="text" placeholder="Username" style={styles.input} />
+                <input type="password" placeholder="Password" style={styles.input}/>
+                <button type="submit" style={styles.submit}>Login</button>
+                <a href="#" style={{
+                    textDecoration: "none",
+                    color: "#494B53",
+                    fontSize: "14px",
+                    marginTop: "10px"
+                }}>Forgot Password?</a>
             </form>
         </div>
     )
@@ -57,10 +76,9 @@ const LoginForm = () => {
 
 const login = () => {
     return(
-        <>
-            <Headings />
+        <div>
             <LoginForm />
-        </>
+        </div>
     )
 }
 
