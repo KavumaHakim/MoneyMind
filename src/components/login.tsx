b2076c4ed79cee26da06d8399865596c0538bb72
Hakim <hakimkavuma99@gmail.com>
2025-05-08 21:12:36 +0300

    refactor: restructure login component and enhance styling

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,26 +1,45 @@
-import Logo from '../assets/react.svg'
+
+import Logo from "../assets/react.svg"
+
 const companyName: string = "YYY STATISTICS";
+interface Styles {
+    container: React.CSSProperties;
+    logo: React.CSSProperties;
+}
+
+const styles: Styles = {
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: "20px",
+        height: "100vh"
+    },
+    logo: {
+        width: "100px",
+        height: "100px"
+    }
+}
+const Headings = () =>{
+    return(
+        <div style={styles.container}>
+            <img src={Logo} style={styles.logo}></img>
+            <h1>{companyName}</h1>
+            
+        </div>
+        
+    )
+}
+
+
+
 const login = () => {
-    const styles: object = {
-        fontSize:'2rem',
-        fontFamily:'century gothic',
-        color:'red',
-        textAlign:'center',
-    }
-    const styleE: object = {
-        display:'flex',
-        gap:'5px',
-        height:'100%',
-        width:'100%'
-    }
     return(
         <>
-            <div style={styleE}>
-                <img src={Logo} alt="logo" />
-                <h1 style={styles}>{companyName}</h1>
-            </div>
+            <Headings />
         </>
     )
 }
 
 export default login
+export {Headings}
