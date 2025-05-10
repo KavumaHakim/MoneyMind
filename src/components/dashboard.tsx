679bd4f908c385bc0d612da020a4bb455d62f547
Hakim <hakimkavuma99@gmail.com>
2025-05-10 20:57:03 +0300

    feat: add react-icons and react-router-dom dependencies; enhance Dashboard component with new layout and InfoCard feature

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,16 +1,84 @@
+import { IoHomeSharp } from "react-icons/io5";
+import type { IconType } from "react-icons";
+import { IoMdNotifications } from "react-icons/io";
+import { FaUserAlt } from "react-icons/fa";
 
 
 const Dashboard = () => {
     return (
-        <div className="grid grid-cols-[100px_1fr]">
-            <div className="h-screen col-span-1 w-full bg-gradient-to-r from-blue-500 to-blue-700 flex items-center justify-center">
+        <div className="grid grid-cols-[200px_1fr] bg-[#080808]">
+            <Sidebar/>
+            <div>
+                <Heading/>
+                    <div className="flex bg-[#202020] w-100% justify-evenly flex-wrap h-45 items-center ml-5 mr-5 rounded-2xl">
+                        <InfoCard cardName="Average" icon={IoHomeSharp} value="766"/>
+                        <InfoCard cardName="Total" icon={IoHomeSharp} value="766"/>
+                        <InfoCard cardName="Students" icon={IoHomeSharp} value="766"/>
+                        <InfoCard cardName="Students" icon={IoHomeSharp} value="766"/>
+                    </div>
             </div>
-            <div className="flex justify-evenly">
-                <h1 className="text-4xl font-extrabold">MoneyMind</h1>
+        </div>
+        )
+}
 
+const Heading = () => {
+    return (
+        <div className="flex justify-between h-15 mt-2">
+            <h1 className="text-5xl font-extrabold self-start ml-1 text-white ">MoneyMin_</h1>
+            <div className="flex gap-5 self-center mr-2">
+                <Notification />
+                <Profile />
+            </div>
+        </div>
+    );
+}
+
+const Sidebar = () => {
+    return(
+        <div className="h-screen w-full bg-black flex flex-col items-center">
+            <div className="flex flex-col gap-5 mt-10">
+                <IoHomeSharp size={50} className="text-[white]" />
+                <IoMdNotifications size={50} className="text-[white]" />
+                <FaUserAlt size={50} className="text-[white]" />
             </div>
         </div>
     )
 }
+
+const Notification = () => {
+    return(
+        <div className="relative">
+            <p className="absolute -top-2 -right-2 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white">2</p>
+            <IoMdNotifications size={50} className="text-[white]" />
+        </div>
+    )
+}
+
+const Profile = () => {
+    return(
+        <div>
+            <FaUserAlt size={50} className="text-[white]" />
+        </div>
+    )
+}
+
+type InfoCardProps = {
+    cardName: string;
+    icon: IconType;
+    value: string;
+};
+
+const InfoCard = ({ cardName, icon, value }: InfoCardProps) => {
+    const Icon = icon;
+    return (
+        <div className="h-35 bg-black rounded-2xl w-50 flex p-2 flex-col ">
+            <Icon size={30} className="text-blue-600" />
+            <h2 className="text-2xl text-white  mt-4">{cardName}</h2>
+            <p>
+                {value}
+            </p>
+        </div>
+    );
+};
 
 export default Dashboard;
