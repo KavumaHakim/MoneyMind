00d493b7f5e7f7a38cde15683d0d5b7deb2f4c7c
Mugabe-Joshua <jshmgb3@gmail.com>
2025-05-13 20:36:04 +0300

    Add backend dependencies

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -11,17 +11,19 @@ const data = [
 
 const Chart = () => {
     return(
-<ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar type="monotone" dataKey="pv" stroke="#8884d8" />
-            <Bar type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </BarChart>
-</ResponsiveContainer>
+    <div className='w-[20%]'>
+        <ResponsiveContainer width="100%" height={250}>
+                <BarChart data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar type="monotone" dataKey="pv" stroke="#8884d8" />
+                    <Bar type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </BarChart>
+        </ResponsiveContainer>
+    </div>
     )
 }
 
