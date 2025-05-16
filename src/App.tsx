7509b1159a7dd453907f18d7dae4da989aff398f
Hakim <hakimkavuma99@gmail.com>
2025-05-16 15:16:24 +0300

    feat: add react-data-table-component dependency and supabase client implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
+// import supabase from './supabase-client';
 import Dashboard from "./Pages/dashboard";
 import Login from "./Pages/login"
 
